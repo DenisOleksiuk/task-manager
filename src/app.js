e203d3a50f5155b2dfa1673894f69e54a1948c63
Denis <denisolexyuk@gmail.com>
2021-08-15 23:07:32 +0300

    refactor: remove unnessesary variable

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 require('./db/mongoose');
 const userRouter = require('./routers/user-router');
 const taskRouter = require('./routers/task-router');
-
 const app = express();
-const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
